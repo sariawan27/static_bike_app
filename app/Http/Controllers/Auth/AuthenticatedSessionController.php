68985539aca44f9cf25b50173a1ee6872c459769
sariawan27 <adhityaeka69@gmail.com>
2025-09-17 10:41:50 +0700

    log login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\Log;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -32,6 +33,11 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        // Simpan ke logs
+        Log::create([
+            'user_id' => Auth::id()
+        ]);
 
         return redirect()->intended(route('dashboard', absolute: false));
     }
