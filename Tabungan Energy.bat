5d9c0489d1709329b930920a5eceec1c27e1ff59
sariawan27 <adhityaeka69@gmail.com>
2025-12-16 09:53:53 +0700

    icon

--- Tabungan Energy.bat
+++ Tabungan Energy.bat
@@ -1,15 +1,24 @@
 @echo off
-echo Menjalankan apache dan mysql...
+echo Menjalankan Apache dan MySQL...
 start "" /B "C:\xampp\apache\bin\httpd.exe"
 start "" /B "C:\xampp\mysql\bin\mysqld.exe"
 
-echo Menjalankan server di jendela baru...
-powershell -WindowStyle Hidden -Command "Start-Process 'php' 'artisan serve'"
+echo Menjalankan Node-RED...
+powershell -WindowStyle Hidden -Command ^
+"Start-Process 'C:\Users\MINI S\AppData\Roaming\npm\node-red.cmd' -WorkingDirectory 'C:\Users\MINI S\AppData\Roaming\npm' -WindowStyle Hidden"
+
+echo Menjalankan Laravel server...
+powershell -WindowStyle Hidden -Command ^
+"Start-Process php 'artisan serve' -WorkingDirectory '%cd%' -WindowStyle Hidden"
+
+echo Menjalankan WebSocket Python (Modbus)...
+powershell -WindowStyle Hidden -Command ^
+"Start-Process python 'ws_realtime.py --port COM5 --unit 1 --ws-port 8765' -WorkingDirectory 'C:\xampp\htdocs\py-modbus-data' -WindowStyle Hidden"
 
 echo Menunggu server siap...
-powershell -WindowStyle Hidden -c "Start-Sleep -Seconds 60"
+powershell -WindowStyle Hidden -Command "Start-Sleep -Seconds 60"
 
-echo Membuka Microsoft Edge...
-start msedge http://localhost:8000
+echo Membuka Microsoft Edge (maximize)...
+start "" msedge --start-maximized http://localhost:8000
 
 pause
